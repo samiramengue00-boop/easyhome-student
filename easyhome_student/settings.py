4415b776bbc6e57d2a467d08999b4d54f54595e6
samiramengue00-boop <samiramengue00@gmail.com>
2026-05-31 13:07:42 +0000

    fix csrf trusted origins

--- easyhome_student/settings.py
+++ easyhome_student/settings.py
@@ -1,17 +1,10 @@
-"""
-Django settings for easyhome_student project.
-"""
-
 from pathlib import Path
 import os
 
 AUTH_USER_MODEL = 'users.Utilisateur'
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# ===== SÉCURITÉ =====
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-ce3b+-a+o$lv47f_^uqd3qyszk^-9tkqnr)b4!1ue*(hhc@v19')
-
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = [
@@ -22,7 +15,12 @@ ALLOWED_HOSTS = [
     '127.0.0.1'
 ]
 
-# ===== APPLICATIONS =====
+CSRF_TRUSTED_ORIGINS = [
+    'https://easyhome-student-production.up.railway.app',
+    'https://*.up.railway.app',
+    'https://*.railway.app',
+]
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -35,7 +33,6 @@ INSTALLED_APPS = [
     'reservations',
 ]
 
-# ===== MIDDLEWARE =====
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
@@ -67,7 +64,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'easyhome_student.wsgi.application'
 
-# ===== BASE DE DONNÉES =====
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -75,21 +71,17 @@ DATABASES = {
     }
 }
 
-# ===== FICHIERS STATIQUES =====
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# ===== FICHIERS MEDIA =====
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-# ===== REDIRECTIONS =====
 LOGIN_REDIRECT_URL = 'accueil'
 LOGOUT_REDIRECT_URL = 'accueil'
 LOGIN_URL = 'connexion'
 
-# ===== EMAIL =====
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT = 587
@@ -97,7 +89,6 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'samiramengue00@gmail.com')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'exdwdxpspdmovlta')
 
-# ===== LANGUE ET FUSEAU HORAIRE =====
 LANGUAGE_CODE = 'fr-fr'
 TIME_ZONE = 'Africa/Dakar'
 USE_I18N = True
